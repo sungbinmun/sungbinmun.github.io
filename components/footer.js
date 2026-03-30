4b9ec306d6a265ee69634f1c9b2e4cabb0aeda4b
Sungbin Mun <sungbinmun@Sungbinui-MacBookPro.local>
2026-03-30 19:22:24 +0900

    Modify to my version

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,17 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Link } from '@chakra-ui/react'
+import NextLink from 'next/link'
 
 const Footer = () => {
   return (
-    <Box align="center" fontSize="sm" opacity={0.5} pt={8}>
-      Copyright {new Date().getFullYear()} Sungbin Mun
+    <Box align="center" fontSize="sm" opacity={0.4} pt={8}>
+      This website is built based on{' '}
+      <Link as={NextLink} href='https://www.craftz.dog/' target='_blank'>
+        Takuya Matsuyama&apos;s website
+      </Link>{' '}
+      and{' '}
+      <Link as={NextLink} href='https://www.changwoon.info' target='_blank'>
+        Changwoon Choi&apos;s website
+      </Link>
     </Box>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import NextLink from 'next/link'
 import {
   Link,
@@ -7,9 +7,7 @@ import {
   Box,
   Button,
   chakra,
-  Collapse,
 } from '@chakra-ui/react'
-import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
 import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
@@ -28,7 +26,6 @@ const PROFILE_WIDTH = 150
 const PROFILE_HEIGHT = 170
 
 const Home = () => {
-  const [showMap, setShowMap] = useState(false);
   return (
   <Layout>
     <Container>
@@ -193,19 +190,6 @@ const Home = () => {
         </BioSection>
       </Section>
       <LogoBanner />
-
-      <Heading
-        onClick={() => setShowMap(!showMap)}
-        display="flex"
-        cursor="pointer"
-        variant="hidden-component"
-      >
-        Traveled countries
-        {showMap ? <ChevronUpIcon /> : <ChevronDownIcon />}
-      </Heading>
-      <Collapse in={showMap} animateOpacity align="center">
-        <iframe src="https://visitedplaces.com/embed/?map=world&projection=geoOrthographic&position=3.5_130.1_36.3_-133.3_-35.8&theme=dark-yellow&width=594&height=435&water=1&graticule=1&names=1&duration=2000&placeduration=100&slider=0&autoplay=0&autozoom=none&autostep=0&home=KR&places=~US_CA_GB_FR_NL_CH_CZ_IT_DE_CN_JP_TH_KH_LA_AU_MY_BE_AT" style={{width: "70%", height: "250px"}}></iframe>
-      </Collapse>
 
       <Box align="center" h="2em">
       </Box>
